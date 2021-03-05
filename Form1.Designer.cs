27dd0ac826bd47082e5b20ebe9b79337b82a54d4
Guillermo Ramirez <gramz@mesoamericana.edu.gt>
2021-03-04 23:28:05 -0600

    Ya realiza la carga de los dos archivos a su lista y muestra cada lista en su respectivo gridview

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridViewEmpleado = new System.Windows.Forms.DataGridView();
+            this.dataGridViewAsistencia = new System.Windows.Forms.DataGridView();
+            this.buttonCargar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmpleado)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAsistencia)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewEmpleado
+            // 
+            this.dataGridViewEmpleado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewEmpleado.Location = new System.Drawing.Point(50, 26);
+            this.dataGridViewEmpleado.Name = "dataGridViewEmpleado";
+            this.dataGridViewEmpleado.Size = new System.Drawing.Size(417, 170);
+            this.dataGridViewEmpleado.TabIndex = 0;
+            // 
+            // dataGridViewAsistencia
+            // 
+            this.dataGridViewAsistencia.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewAsistencia.Location = new System.Drawing.Point(50, 280);
+            this.dataGridViewAsistencia.Name = "dataGridViewAsistencia";
+            this.dataGridViewAsistencia.Size = new System.Drawing.Size(417, 150);
+            this.dataGridViewAsistencia.TabIndex = 1;
+            // 
+            // buttonCargar
+            // 
+            this.buttonCargar.Location = new System.Drawing.Point(173, 499);
+            this.buttonCargar.Name = "buttonCargar";
+            this.buttonCargar.Size = new System.Drawing.Size(154, 53);
+            this.buttonCargar.TabIndex = 2;
+            this.buttonCargar.Text = "Cargar Datos";
+            this.buttonCargar.UseVisualStyleBackColor = true;
+            this.buttonCargar.Click += new System.EventHandler(this.buttonCargar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1041, 593);
+            this.Controls.Add(this.buttonCargar);
+            this.Controls.Add(this.dataGridViewAsistencia);
+            this.Controls.Add(this.dataGridViewEmpleado);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmpleado)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAsistencia)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewEmpleado;
+        private System.Windows.Forms.DataGridView dataGridViewAsistencia;
+        private System.Windows.Forms.Button buttonCargar;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,66 @@ namespace ControlSueldos
 {
     public partial class Form1 : Form
     {
+        List<Empleado> empleados = new List<Empleado>();
+        List<Asistencia> asistencias = new List<Asistencia>();
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonCargar_Click(object sender, EventArgs e)
+        {
+           
+              
+
+            FileStream stream = new FileStream("empleado.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+                
+            while (reader.Peek() > -1)                
+            {
+                //Declaramos un objeto empleado de la clase Empleado
+                //para ingresarle los datos leidos del archivo
+                //y luego guardar el empleado en la lista empleados
+                Empleado empleado = new Empleado();
+                empleado.NoEmpleado = Convert.ToInt32 (reader.ReadLine());
+                empleado.Nombre = reader.ReadLine();
+                empleado.SueldoHora = float.Parse(reader.ReadLine());
+
+            empleados.Add(empleado);
+
+            }            
+            reader.Close();
+
+            FileStream stream2 = new FileStream("asistencia.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader2 = new StreamReader(stream2);
+
+            while (reader2.Peek() > -1)
+            {
+                //Declaramos un objeto asistencia de la clase Asistencia
+                //para ingresarle los datos leidos del archivo
+                //y luego guardar las asitencia en la lista asistencias
+                Asistencia asistencia = new Asistencia();
+
+                asistencia.NoEmpleado = Convert.ToInt32(reader2.ReadLine());
+                asistencia.HorasMes = Convert.ToInt32(reader2.ReadLine());
+                asistencia.Mes = reader2.ReadLine();
+
+                asistencias.Add(asistencia);
+            }            
+            reader2.Close();
+
+            //Desplegar en los gridviews
+
+            dataGridViewEmpleado.DataSource = null;
+            dataGridViewEmpleado.DataSource = empleados;
+            dataGridViewEmpleado.Refresh();
+
+            dataGridViewAsistencia.DataSource = null;
+            dataGridViewAsistencia.DataSource = asistencias;
+            dataGridViewAsistencia.Refresh();
+
         }
     }
 }
