b072b9fcfb937e0063cc847ddc6ffb04db7b092c
Guillermo Ramirez <gramz@mesoamericana.edu.gt>
2021-03-04 23:30:56 -0600

    Se le colocaron etiquetas a cada gridview

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,8 +31,13 @@
             this.dataGridViewEmpleado = new System.Windows.Forms.DataGridView();
             this.dataGridViewAsistencia = new System.Windows.Forms.DataGridView();
             this.buttonCargar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmpleado)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAsistencia)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridViewEmpleado
@@ -61,11 +66,50 @@
             this.buttonCargar.UseVisualStyleBackColor = true;
             this.buttonCargar.Click += new System.EventHandler(this.buttonCargar_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(547, 215);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(424, 150);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Datos del Empleado";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 254);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Datos de la asistencia";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(533, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Reporte de sueldos por mes";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1041, 593);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.buttonCargar);
             this.Controls.Add(this.dataGridViewAsistencia);
             this.Controls.Add(this.dataGridViewEmpleado);
@@ -73,7 +117,9 @@
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmpleado)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAsistencia)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -82,6 +128,10 @@
         private System.Windows.Forms.DataGridView dataGridViewEmpleado;
         private System.Windows.Forms.DataGridView dataGridViewAsistencia;
         private System.Windows.Forms.Button buttonCargar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
